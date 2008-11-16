73a282ca11ef795702f49e1384fdd0f9b0bc3cc9
mcerdeiro <mcerdeiro@gmail.com>
2008-11-16 17:34:14 +0000

    add arm and s12x memmap files
    git-svn-id: https://svn.code.sf.net/p/opensek/code/trunk@61 73ee5bea-b2a1-4b4e-8992-c3b1447581ab

---
+++ OpenGEN/inc/arm/MemMap_Section_Arch.h
@@ -0,0 +1,71 @@
+/* Copyright 2008, Mariano Cerdeiro
+ *
+ * This file is part of OpenSEK.
+ *
+ * OpenSEK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenSEK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenSEK. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef _MEMMAP_SECTION_ARCH_H_
+#define _MEMMAP_SECTION_ARCH_H_
+/** \brief OpenSEK Posix Section Mapping Header File
+ **
+ ** \file arm/MemMap_Section_Arch.h
+ **
+ **/
+
+/** \addtogroup OpenGEN
+ ** @{ */
+/** \addtogroup OpenGEN_Global
+ ** \ingroup OpenGEN
+ ** @{ */
+
+/*
+ * Initials	  Name
+ * ---------------------------
+ * MaCe			 Mariano Cerdeiro
+ */
+
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * v0.1.0 20081116 MaCe	initial version
+ */
+
+#ifdef START_SECTION_CODE
+	#undef START_SECTION_CODE
+	#undef MEMMAP_ERROR
+#endif
+
+#ifdef STOP_SECTION_CODE
+	#undef STOP_SECTION_CODE
+	#undef MEMMAP_ERROR
+#endif
+
+#ifdef START_SECTION_VAR
+   #undef START_SECTION_VAR
+   #undef MEMMAP_ERROR
+#endif
+
+#ifdef STOP_SECTION_VAR
+   #undef STOP_SECTION_VAR
+   #undef MEMMAP_ERROR
+#endif
+
+
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+#endif /* #ifndef _MEMMAP_SECTION_ARCH_H_ */
+
---
+++ OpenGEN/inc/s12x/MemMap_Section_Arch.h
@@ -0,0 +1,75 @@
+/* Copyright 2008, Mariano Cerdeiro
+ *
+ * This file is part of OpenSEK.
+ *
+ * OpenSEK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenSEK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenSEK. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef _MEMMAP_SECTION_ARCH_H_
+#define _MEMMAP_SECTION_ARCH_H_
+/** \brief OpenSEK Posix Section Mapping Header File
+ **
+ ** \file posix/MemMap_Section_Arch.h
+ **
+ **/
+
+/** \addtogroup OpenGEN
+ ** @{ */
+/** \addtogroup OpenGEN_Global
+ ** \ingroup OpenGEN
+ ** @{ */
+
+/*
+ * Initials	  Name
+ * ---------------------------
+ * MaCe			 Mariano Cerdeiro
+ */
+
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * v0.1.0 20081114 MaCe	initial version
+ */
+
+#ifdef START_SECTION_CODE
+	#pragma MEM_ATTRIB_OSEK_CODE_BEGIN
+	#undef START_SECTION_CODE
+	#undef MEMMAP_ERROR
+#endif
+
+#ifdef STOP_SECTION_CODE
+	#pragma MEM_ATTRIB_OSEK_CODE_END
+	#undef STOP_SECTION_CODE
+	#undef MEMMAP_ERROR
+#endif
+
+#ifdef START_SECTION_VAR
+	#pragma MEM_ATTRIB_OSEK_RAM_BEGIN
+   #undef START_SECTION_VAR
+   #undef MEMMAP_ERROR
+#endif
+
+#ifdef STOP_SECTION_VAR
+	#pragma MEM_ATTRIB_OSEK_RAM_END
+   #undef STOP_SECTION_VAR
+   #undef MEMMAP_ERROR
+#endif
+
+
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+#endif /* #ifndef _MEMMAP_H_ */
+
