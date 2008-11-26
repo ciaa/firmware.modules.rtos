5de18f3eee250162f4bd48c1caa669b9a24dfd83
mcerdeiro <mcerdeiro@gmail.com>
2008-11-26 21:22:29 +0000

    check first version of OpenDRVs
    git-svn-id: https://svn.code.sf.net/p/opensek/code/trunk@69 73ee5bea-b2a1-4b4e-8992-c3b1447581ab

---
+++ OpenDRV/Clk/inc/clk.h
@@ -0,0 +1,77 @@
+/* Copyright 2008, Mariano Cerdeiro
+ *
+ * This file is part of OpenDRV.
+ *
+ * OpenDRV is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenDRV is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenSEK. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef _CLK_H_
+#define _CLK_H_
+/** \brief OpenDRV Clock Header File
+ **
+ ** This file shall be included by all files using any OpenDRV Clock API.
+ **
+ ** \file Clk.h
+ **
+ **/
+
+/** \addtogroup OpenDRV
+ ** @{ */
+/** \addtogroup OpenDRV_Clk
+ ** \ingroup OpenDRV
+ ** @{ */
+
+/*
+ * Initials     Name
+ * ---------------------------
+ * MaCe			 Mariano Cerdeiro
+ */
+
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * v0.1.0 20081126 MaCe	initial version
+ */
+
+/*==================[inclusions]=============================================*/
+#include "Gen_Types.h"
+#include "Clk_Cfg.h"
+#include "Clk_Arch.h"
+
+/*==================[macros]=================================================*/
+
+/*==================[typedef]================================================*/
+typedef uint8 Clk_ReturnType;
+
+typedef struct {
+	Clk_ConfigArchType Clk_Arch;
+} Clk_ConfigType;
+
+typedef Clk_ConfigType* Clk_ConfigRefType;
+
+/*==================[external data declaration]==============================*/
+
+/*==================[external functions declaration]=========================*/
+extern Clk_ReturnType Clk_Init(Clk_ConfigRefType config);
+
+extern Clk_RetunrType Clk_ReInit(Clk_ConfigRefType config);
+
+extern Clk_ReturnType Clk_DeInit();
+
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+#endif /* #ifndef _CLK_H_ */
+
---
+++ OpenDRV/StartUp/arm/StartUp.c
@@ -0,0 +1,61 @@
+/* Copyright 2008, Mariano Cerdeiro
+ *
+ * This file is part of OpenDRV
+ *
+ * OpenDRV is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenDRV is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenSEK. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/** \brief OpenDRV StartUp c file for arm
+ **
+ ** \file arm/SrtartUp.c
+ ** \arch arm
+ **/
+
+/** \addtogroup OpenDRV
+ ** @{ */
+/** \addtogroup OpenDRV_StartUp
+ ** @{ */
+
+/*
+ * Initials     Name
+ * ---------------------------
+ * MaCe         Mariano Cerdeiro
+ */
+
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * 20081126 v0.1.0 MaCe       - initial version based on software without
+ *                              author.
+ */
+
+/*==================[inclusions]=============================================*/
+
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data declaration]==============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+/*==================[external functions definition]==========================*/
+
+/*==================[end of file]============================================*/
+
