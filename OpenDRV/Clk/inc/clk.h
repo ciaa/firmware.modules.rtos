8bc31bc135d8d8947b157b3aed7d39c32730ff68
mcerdeiro <mcerdeiro@gmail.com>
2008-11-27 21:26:29 +0000

    correct copyright
    git-svn-id: https://svn.code.sf.net/p/opensek/code/trunk@72 73ee5bea-b2a1-4b4e-8992-c3b1447581ab

--- OpenDRV/Clk/inc/clk.h
+++ OpenDRV/Clk/inc/clk.h
@@ -1,13 +1,13 @@
 /* Copyright 2008, Mariano Cerdeiro
  *
- * This file is part of OpenDRV.
+ * This file is part of OpenSEK.
  *
- * OpenDRV is free software: you can redistribute it and/or modify
+ * OpenSEK is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * OpenDRV is distributed in the hope that it will be useful,
+ * OpenSEK is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
