059c103785392ad69e4fdf4f2f64d647a07a484b
mcerdeiro <mcerdeiro@gmail.com>
2009-01-25 13:21:05 +0000

    update/add io driver for arm/lpc2468 (basic structure)
    git-svn-id: https://svn.code.sf.net/p/opensek/code/trunk@111 73ee5bea-b2a1-4b4e-8992-c3b1447581ab

---
+++ OpenDRV/Io/gen/inc/Io_Cfg.h.php
@@ -0,0 +1,72 @@
+/********************************************************
+ * DO NOT CHANGE THIS FILE, IT IS GENERATED AUTOMATICALY*
+ ********************************************************/
+
+/* Copyright 2008, Mariano Cerdeiro
+ *
+ * This file is part of OpenSEK.
+ *
+ * OpenSEK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenSEK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenSEK. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+<?php
+/** \brief OpenDRV File to be Generated
+ **
+ ** \file Io_Cfg.h.php
+ **/
+?>
+
+#ifndef _IO_CFG_H_
+#define _IO_CFG_H_
+/** \brief OpenDRV IO Generated Configuration Header File
+ **
+ ** This file contents the generated configuration of the IO Driver
+ **
+ ** \file Io_Cfg.h
+ **/
+
+/** \addtogroup OpenDRV
+ ** @{ */ 
+/** \addtogroup OpenDRV_Io
+ ** \ingroup OpenDRV
+ ** @{ */
+
+/*
+ * Initials     Name
+ * ---------------------------
+ * MaCe			 Mariano Cerdeiro
+ */
+
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * v0.1.0 20090125 MaCe	initial version
+ */  
+
+/*==================[inclusions]=============================================*/
+
+/*==================[macros]=================================================*/
+
+/*==================[typedef]================================================*/
+
+/*==================[external data declaration]==============================*/
+
+/*==================[external functions declaration]=========================*/
+
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+#endif /* #ifndef _IO_CFG_H_ */
+
