1af4bb853f3ad733b0cb936fba2220f9cb5e7ebe
mcerdeiro <mcerdeiro@gmail.com>
2009-01-25 18:12:47 +0000

    update add io driver for arm
    git-svn-id: https://svn.code.sf.net/p/opensek/code/trunk@115 73ee5bea-b2a1-4b4e-8992-c3b1447581ab

--- OpenDRV/Io/inc/io.h
+++ OpenDRV/Io/inc/io.h
@@ -17,21 +17,25 @@
  *
  */
 
-#ifndef _IO_H_
-#define _IO_H_
-/** \brief OpenDRV IO Header File
+#ifndef _IO_INTERNAL_H_
+#define _IO_INTERNAL_H_
+/** \brief Io Internal Header File
  **
- ** This file shall be included by all files using any OpenDRV IO API.
+ ** Io Internal Header File
  **
- ** \file io.h
+ ** \file Io_Internal.h
  **
  **/
 
 /** \addtogroup OpenDRV
  ** @{ */
-/** \addtogroup OpenDRV_Clk
+/** \addtogroup OpenDRV_Io
  ** \ingroup OpenDRV
  ** @{ */
+/** \addtogroup OpenDRV_Io_Internal
+ ** \ingroup OpenDRV_Io
+ ** @{ */
+
 
 /*
  * Initials     Name
@@ -42,137 +46,30 @@
 /*
  * modification history (new versions first)
  * -----------------------------------------------------------
- * v0.1.0 20081126 MaCe	initial version
- */
+ * v0.1.0 20090125 MaCe	initial version
+ */  
 
 /*==================[inclusions]=============================================*/
-#include "Types.h"
-#include "Io_Cfg.h"
-#include "Io_Arch.h"
+#include "io.h"
 
 /*==================[macros]=================================================*/
-/** \brief IO driver set port service
- **
- ** This IO driver service shall be called to set a port to a specified value.
- ** The bits not configured of this port will stay unchaged. Writing to an
- ** input is not defined.
- **
- ** \param[in] port indicates the port to be set
- ** \param[in] value indicate the value to be set to this port
- ** \return IO_E_OK is always returned
- **/
-#define Io_SetPort(port, value) 				\
-	IO_E_OK;											\
-	{													\
-		Io_SetPort_Arch(port, value, mask);	\
-	}
 
-/** \brief IO driver set pin service
- **
- ** This IO driver service shall be called to set a pint to a specific value.
- **
- ** \param[in] pin indicates the pin to be set
- ** \param[in] value indicates the value to be set to this pin
- ** \return IO_E_OK is always returned
- **/
-#define Io_SetPin(pin, value)					\
-	IO_E_OK;											\
-	{													\
-		Io_SetPort_Arch(port, value);			\
-	}
-
-/** \brief IO driver get port service
- **
- ** This IO driver service shall be called to get the status of a specific port.
- **
- ** \param[in] port inidcates the port to be readed
- ** \param[out] value the value of the readed port is set to this variable
- ** \return IO_E_OK is always returned
- **/
-#define Io_GetPort(port, value)				\
-	IO_E_OK;											\
-	{													\
-		Io_GetPort_Arch(port, value);			\
-	}
-
-/** \brief IO driver get pin service 
- **
- ** This IO driver service shall be called to get the state of a specific pin
- **
- ** \param[in] pin indicates the pin to be readed
- ** \param[out] value the status of the readed port is set to this variable
- ** \return IO_E_OK is always returned
- **/
-#define Io_GetPin(pin, value)					\
-	IO_E_OK;											\
-	{													\
-		Io_GetPint_Arch(pin, value)			\
-	}													
-		
 /*==================[typedef]================================================*/
-/** \brief IO driver return type definition */
-typedef uint8 Io_ReturnType;
-
-/** \brief IO driver configuration type definition */
-typedef struct {
-	Io_ConfigArchType Io_Arch;
-} Io_ConfigType;
-
-/** \brief IO driver configuration reference type definition */
-typedef Io_ConfigType* Io_ConfigRefType;
-
-/** \brief IO driver port type configuration */
-typedef uint8 Io_PortType;
-
-/** \brief IO driver port value type definition */
-#if   (IO_PORT_BITS_WIDTH == 8)
-typedef uint8 Io_PortValue;
-#elif (IO_PORT_BITS_WIDTH == 16)
-typedef uint16 Io_PortValue;
-#elif (IO_PORT_BITS_WIDHT == 32)
-typedef uint32 Io_PortValue;
-#else
-#error invalid IO_PORT_BITS_WIDHT value
-#endif
 
 /*==================[external data declaration]==============================*/
 
 /*==================[external functions declaration]=========================*/
-/** \brief IO driver init service
- **
- ** This IO driver service shall be called to initialize the IO driver. Calls
- ** to any other IO driver service before initialization is not allowed.
- **
- ** \param[in] config pointer to the IO driver configuration
- ** \return IO_E_OK	if no error occurs
- ** \return IO_E_PARAM if the configuration is invalid
- ** \return IO_E_NOTOK if other error occurs
- **/
-extern Io_ReturnType Io_Init(const Io_ConfigRefType config);
+/** TODO */
+/* #define OpenDRV_IO_START_SEC_CODE
+ * #include "MemMap.h" */
 
-/** \brief IO driver re init service
- **
- ** This IO driver service shall be called to re initialize the IO driver.
- ** This services allows the user to change the port configuration on the go.
- **
- ** \param[in] config pointer to the new IO driver configuration
- ** \return IO_E_OK if no error occurs
- ** \return IO_E_PARAM if the configuration is invalid
- ** \return IO_E_NOTOK if other error occurs
- **/
-extern Io_ReturnType Io_ReInit(const Io_ConfigRefType config);
+/** TODO */
+/* #define OpenDRV_IO_STOP_SEC_CODE
+ * #include "MemMap.h" */
 
-/** \brief IO driver de-init service
- **
- ** This IO driver service shall be called to de-initialize the IO driver.
- **
- ** \return IO_E_OK if no error occurs
- ** \return IO_E_NOTOK if any error occurs
- **/
-extern Io_ReturnType Io_DeInit();
-
+/** @} doxygen end group definition */
 /** @} doxygen end group definition */
 /** @} doxygen end group definition */
 /*==================[end of file]============================================*/
-#endif /* #ifndef _CLK_H_ */
+#endif /* #ifndef _IO_INTERNAL_H_ */
 
