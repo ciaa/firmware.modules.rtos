b4dfe469bf9d9d128e3aa168c214dbce93873f53
mcerdeiro <mcerdeiro@gmail.com>
2009-02-19 19:08:11 +0000

    2617248 add AT91 arch for the Dio driver
    git-svn-id: https://svn.code.sf.net/p/opensek/code/trunk@170 73ee5bea-b2a1-4b4e-8992-c3b1447581ab

---
+++ OpenDRV/Dio/inc/arm/at91/Dio_Arch.h
@@ -0,0 +1,96 @@
+/* Copyright 2008, Mariano Cerdeiro
+ *
+ * This file is part of OpenSEK.
+ *
+ * OpenSEK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenSEK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenSEK. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef _DIO_ARCH_H_
+#define _IO_ARCH_H_
+/** \brief DIO Arch Header File
+ **
+ ** DIO Arch Header File
+ **
+ ** \file Dio_Arch.h
+ **
+ **/
+
+/** \addtogroup OpenDRV
+ ** @{ */
+/** \addtogroup OpenDRV_Dio
+ ** \ingroup OpenDRV
+ ** @{ */
+/** \addtogroup OpenDRV_Dio_Internal
+ ** \ingroup OpenDRV_Dio
+ ** @{ */
+
+
+/*
+ * Initials     Name
+ * ---------------------------
+ * MaCe			 Mariano Cerdeiro
+ */
+
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * 20090219 v0.1.0 MaCe	initial version
+ */  
+
+/*==================[inclusions]=============================================*/
+#include "at91.h"
+
+/*==================[macros]=================================================*/
+
+/*==================[typedef]================================================*/
+typedef struct {
+	uint8 Config;
+} Dio_ConfigArchType;
+
+/** \brief Channel type definition */
+/* \req DIO015 Parameters of type Dio_ChannelType contain the numeric ID of a
+ * DIO channel. The mapping of the ID is implementation specific but not
+ * configurable. */
+typedef uint8 Dio_ChannelType;
+
+/** \brief Port type definition */
+/* \req DIO018 Parameters of type Dio_PortType contain the numeric ID of a DIO
+ * port. The mapping of ID is implementation specific but not configurable. */
+typedef uint8 Dio_PortType;
+
+/** \brief Channel Group type definition */
+/* \req DIO021 Dio_ChannelGroupType is the type for the definition of a channel
+ * group, which consists of several adjoining channels within a port. */
+typedef uint8 Dio_ChannelGroupType;
+
+/** \brief Level type definition */
+/* \req  DIO023 Dio_LevelType is the type for the possible levels that a DIO
+ * channel can have (input or output). */
+typedef uint8 Dio_LevelType;
+
+/** \brief Port Level type definition */
+/* \req DIO024 Dio_PortLevelType is the type for the value of a DIO port. */
+typedef uint32 Dio_PortLevelType;
+
+/*==================[external data declaration]==============================*/
+
+/*==================[external functions declaration]=========================*/
+
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+#endif /* #ifndef _DIO_ARCH_H_ */
+
---
+++ OpenDRV/Dio/src/arm/at91/Dio_Init_Arch.c
@@ -0,0 +1,86 @@
+/* Copyright 2008, Mariano Cerdeiro
+ *
+ * This file is part of OpenSEK.
+ *
+ * OpenSEK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenSEK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenSEK. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/** \brief OpenDRV DIO Init Arch implementation file
+ **
+ ** This file implements the Dio_Init_Arch API
+ **
+ ** \file Dio_Init_Arch.c
+ **
+ **/
+
+/** \addtogroup OpenDRV
+ ** @{ */
+/** \addtogroup OpenDRV_Dio
+ ** \ingroup OpenDRV
+ ** @{ */
+/** \addtogroup OpenDRV_Dio_Internal
+ ** \ingroup OpenDRV_Dio
+ ** @{ */
+
+/*
+ * Initials     Name
+ * ---------------------------
+ * MaCe         Mariano Cerdeiro
+ */
+
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * 20090213 v0.1.1 MaCe raname Io driver to Dio
+ * 20090125 v0.1.0 MaCe       - initial version
+ */
+
+/*==================[inclusions]=============================================*/
+#include "Dio_Internal.h"
+
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data declaration]==============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[external data definition]===============================*/
+
+/*==================[internal functions definition]==========================*/
+
+/*==================[external functions definition]==========================*/
+/** TODO */
+/* #define OpenDRV_DIO_START_SEC_CODE
+ * #include "MemMap.h" */
+
+Dio_ReturnType Dio_Init_Arch
+(
+	Dio_ConfigRefType config
+)
+{
+	/** TODO */
+}
+
+/** TODO */
+/* #define OpenDRV_DIO_STOP_SEC_CODE
+ * #include "MemMap.h" */
+
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+
