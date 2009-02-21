9f98c3e775c1d9fcb08a1716cd3c4f9e35c9a9a7
mcerdeiro <mcerdeiro@gmail.com>
2009-02-21 19:06:28 +0000

    2552346 remove interrupt functions of main.c of the arm example
    git-svn-id: https://svn.code.sf.net/p/opensek/code/trunk@176 73ee5bea-b2a1-4b4e-8992-c3b1447581ab

--- examples/armexample/src/main.c
+++ examples/armexample/src/main.c
@@ -164,23 +164,5 @@ void SetLed(int led, int state)
 		}
 	}
 }
-/*  Stubs for various interrupts (may be replaced later)  */
-/*  ----------------------------------------------------  */
-
-void IRQ_Routine (void) {
-	while (1) ;	
-}
-
-void FIQ_Routine (void)  {
-	while (1) ;	
-}
 
 
-void SWI_Routine (void)  {
-	while (1) ;	
-}
-
-
-void UNDEF_Routine (void) {
-	while (1) ;	
-}
