91376853cde6e529af227ffb716c273cac771548
mcerdeiro <mcerdeiro@gmail.com>
2009-03-17 19:17:37 +0000

    2549968 update eth.h to Eth.h
    git-svn-id: https://svn.code.sf.net/p/opensek/code/trunk@263 73ee5bea-b2a1-4b4e-8992-c3b1447581ab

---
+++ OpenDRV/Eth/inc/Eth.h
@@ -0,0 +1,97 @@
+/* Copyright 2008, 2009, Mariano Cerdeiro
+ *
+ * This file is part of OpenSEK.
+ *
+ * OpenSEK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *             
+ * Linking OpenSEK statically or dynamically with other modules is making a
+ * combined work based on OpenSEK. Thus, the terms and conditions of the GNU
+ * General Public License cover the whole combination.
+ *
+ * In addition, as a special exception, the copyright holders of OpenSEK give
+ * you permission to combine OpenSEK program with free software programs or
+ * libraries that are released under the GNU LGPL and with independent modules
+ * that communicate with OpenSEK solely through the OpenSEK defined interface. 
+ * You may copy and distribute such a system following the terms of the GNU GPL
+ * for OpenSEK and the licenses of the other code concerned, provided that you
+ * include the source code of that other code when and as the GNU GPL requires
+ * distribution of source code.
+ *
+ * Note that people who make modified versions of OpenSEK are not obligated to
+ * grant this special exception for their modified versions; it is their choice
+ * whether to do so. The GNU General Public License gives permission to release
+ * a modified version without this exception; this exception also makes it
+ * possible to release a modified version which carries forward this exception.
+ * 
+ * OpenSEK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenSEK. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef _ETH_H_
+#define _ETH_H_
+/** \brief OpenDRV Ethernet Header File
+ **
+ ** This file shall be included by all files using any OpenDRV Ethernet API.
+ **
+ ** \file eth.h
+ **
+ **/
+
+/** \addtogroup OpenDRV
+ ** @{ */
+/** \addtogroup OpenDRV_Eth
+ ** \ingroup OpenDRV
+ ** @{ */
+
+/*
+ * Initials     Name
+ * ---------------------------
+ * MaCe			 Mariano Cerdeiro
+ */
+
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * v0.1.0 20081129 MaCe	initial version
+ */
+
+/*==================[inclusions]=============================================*/
+#include "Gen_Types.h"
+#include "Eth_Cfg.h"
+#include "Eth_Arch.h"
+
+/*==================[macros]=================================================*/
+
+/*==================[typedef]================================================*/
+
+/*==================[external data declaration]==============================*/
+
+/*==================[external functions declaration]=========================*/
+extern void Eth_ipchksum(void);
+
+extern void Eth_tcpchksum(void);
+
+extern void Eth_add32(void);
+
+extern void Eth_Send(void);
+
+extern void Eth_Receive(void);
+
+extern void Eth_Transmit(void);
+
+extern void Eth_MainFunction(void);
+
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+#endif /* #ifndef _ETH_H_ */
+
