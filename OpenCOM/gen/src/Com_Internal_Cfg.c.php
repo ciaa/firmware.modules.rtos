9f298ed860a7ac42675e0fdc4d55e8552bae7219
mcerdeiro <mcerdeiro@gmail.com>
2009-07-08 18:09:33 +0000

    2703411 update opencom
    git-svn-id: https://svn.code.sf.net/p/opensek/code/trunk@445 73ee5bea-b2a1-4b4e-8992-c3b1447581ab

---
+++ OpenCOM/gen/src/Com_Internal_Cfg.c.php
@@ -0,0 +1,109 @@
+/********************************************************
+ * DO NOT CHANGE THIS FILE, IT IS GENERATED AUTOMATICALY*
+ ********************************************************/
+
+/* Copyright 2008, 2009, Mariano Cerdeiro
+ *
+ * This file is part of FreeOSEK.
+ *
+ * FreeOSEK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *             
+ * Linking FreeOSEK statically or dynamically with other modules is making a
+ * combined work based on FreeOSEK. Thus, the terms and conditions of the GNU
+ * General Public License cover the whole combination.
+ *
+ * In addition, as a special exception, the copyright holders of FreeOSEK give
+ * you permission to combine FreeOSEK program with free software programs or
+ * libraries that are released under the GNU LGPL and with independent modules
+ * that communicate with FreeOSEK solely through the FreeOSEK defined interface. 
+ * You may copy and distribute such a system following the terms of the GNU GPL
+ * for FreeOSEK and the licenses of the other code concerned, provided that you
+ * include the source code of that other code when and as the GNU GPL requires
+ * distribution of source code.
+ *
+ * Note that people who make modified versions of FreeOSEK are not obligated to
+ * grant this special exception for their modified versions; it is their choice
+ * whether to do so. The GNU General Public License gives permission to release
+ * a modified version without this exception; this exception also makes it
+ * possible to release a modified version which carries forward this exception.
+ * 
+ * FreeOSEK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with FreeOSEK. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/** \brief OpenCOM Generated Configuration Implementation File
+ **
+ ** \file Com_Internal_Cfg.c
+ **/
+
+/** \addtogroup OpenCOM
+ ** @{ */
+/** \addtogroup OpenCOM_Internal
+ ** @{ */
+
+/*
+ * Initials     Name
+ * ---------------------------
+ * MaCe         Mariano Cerdeiro
+ */
+
+/*
+ * modification history (new versions first)
+ * -----------------------------------------------------------
+ * 20090708 v0.1.0 MaCe initial version
+ */
+
+/*==================[inclusions]=============================================*/
+#include "Com_Internal.h"
+
+/*==================[macros and definitions]=================================*/
+
+/*==================[internal data declaration]==============================*/
+
+/*==================[internal functions declaration]=========================*/
+
+/*==================[internal data definition]===============================*/
+
+/*==================[external data definition]===============================*/
+<?php
+$messages = $config->getList("/COM","MESSAGE");
+$count = 0;
+print "/** \brief Definition of Send Messages Objects */\n";
+print "const Com_TxMessageObjectType Com_TxMessageObjects[] = {\n";
+foreach ($messages as $msg)
+{
+	$msgprop = $config->getValue("/COM/" . $msg,"MESSAGEPROPERTY");
+	if ( strpos($msgprop,"SEND") > -1 )
+	{
+		if ($count > 0)
+		{
+			print ",\n";
+		}
+		print "	/** \brief Send Message Object: $msg */\n";
+		print "	{\n";
+		print "		\n";
+		print "		\n";
+		print "	}";
+		$count++;
+	}
+}
+print "\n}\n";
+?>
+
+/*==================[internal functions definition]==========================*/
+
+/*==================[external functions definition]==========================*/
+
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+
