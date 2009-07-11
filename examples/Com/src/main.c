a5ee9cd93fe101b632fafc04db9830098d1db34c
mcerdeiro <mcerdeiro@gmail.com>
2009-07-11 16:47:12 +0000

    2703413 update osekcom example
    git-svn-id: https://svn.code.sf.net/p/opensek/code/trunk@450 73ee5bea-b2a1-4b4e-8992-c3b1447581ab

--- examples/Com/src/main.c
+++ examples/Com/src/main.c
@@ -61,19 +61,12 @@
  */
 
 /*==================[inclusions]=============================================*/
-#include "os.h"		/* OSEK header file */
-#include "Mcu.h"		/* MCU Driver header file */
-#include "Dio.h"		/* DIO Driver header file */
+#include "os.h"		/* OSEK RTOS header file */
+#include "com.h"		/* OSEK COM header file */
 
 /*==================[macros and definitions]=================================*/
-#define SET_LED0(val)	Dio_WriteChannel(LED0, (val == 0 ) ? DIO_LOW : DIO_HIGH)
-#define SET_LED1(val)	Dio_WriteChannel(LED1, (val == 0 ) ? DIO_LOW : DIO_HIGH)
-
-#define GET_BUT0()		Dio_ReadChannel(BUT0)
-#define GET_BUT1()		Dio_ReadChannel(BUT1)
 
 /*==================[internal data declaration]==============================*/
-uint8 led1;
 
 /*==================[internal functions declaration]=========================*/
 
@@ -98,6 +91,8 @@ int main
 {	
 	/* Start OSEK */
 	StartOS(AppMode1);
+
+	while(1);
 }
 
 /** \brief Init Task
@@ -107,15 +102,6 @@ int main
  **/
 TASK(InitTask)
 {
-	/* init MCU Driver */
-	Mcu_Init((Mcu_ConfigRefType)NULL);
-
-	/* init Clock */
-	(void)Mcu_InitClock((Mcu_ClockType)0);
-
-	/* init DIO Driver */
-	(void)Dio_Init((Dio_ConfigRefType)NULL);
-
 	/* start cyclic alarm to activate task LedsTask every 250ms */
 	SetRelAlarm(ActivateLedsTask, 250, 250);
 
@@ -136,31 +122,6 @@ TASK(InitTask)
  **/
 TASK(LedsTask)
 {
-	static uint8 led0 = 0;
-	
-	/* check actual state of the led */
-	switch (led0) {
-		case 0:
-			/* set the led off */
-			SET_LED0(0);
-			/* save led 0 status */
-			led0 = 1;
-			break;
-		case 1:
-			/* set the led on */
-			SET_LED0(1);
-			/* save led 0 status */
-			led0 = 0;
-			break;
-		default:
-			/* shall never happens */
-			break;
-	}
-
-	/* set or clear the led1 depending on led1 variable */
-	/* led1 variable will be set on the ButtonsTask Task */
-	SET_LED1(led1);
-
 	/* Terminate Leds Task */
 	TerminateTask();
 }
@@ -171,20 +132,6 @@ TASK(LedsTask)
  **/
 TASK(ButtonsTask)
 {
-
-	/* check if Button 0 is low */
-	if ( GET_BUT0() == DIO_LOW )
-	{
-		/* turn off the led */
-		led1 = 1;
-	}
-	/* check if Button 1 is low */
-	if ( GET_BUT1() == DIO_LOW )
-	{
-		/* turn on the led */
-		led1 = 0;
-	}
-
 	/* Terminate Buttons Task */
 	TerminateTask();
 }
