2eaed799eac7ed4596d5e0396276cdf9dfcf20f9
mcerdeiro <mcerdeiro@gmail.com>
2015-08-10 15:17:49 +0200

    improve use chip.h

--- src/cortexM4/StartOs_Arch_SysTick.c
+++ src/cortexM4/StartOs_Arch_SysTick.c
@@ -57,6 +57,7 @@
 
 /*==================[inclusions]=============================================*/
 #include "Os_Internal_Arch_Cpu.h"
+#include "ciaaPlatforms.h"
 #if (CPU == lpc4337)
 #include "chip.h"
 #endif
