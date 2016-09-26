70c4b43872fe6d8223bf833ed7cc1ff9702376e7
Gerardo Puga <glpuga@gmail.com>
2016-09-26 01:22:13 -0300

    Added register information for the APBUART IP core to grlib.h

--- inc/sparc/leon3/grlib.h
+++ inc/sparc/leon3/grlib.h
@@ -60,6 +60,49 @@
 /*==================[macros]=================================================*/
 
 
+/*
+ *
+ * INTERNAL MACROS
+ *
+ * */
+
+#define GRLIB_GET_FIELD(registerValue, fieldWidth, fieldFirstBit)       ((((uint32)(registerValue)) >> (fieldFirstBit)) & ((1 << (fieldWidth)) - 1))
+#define GRLIB_SHIFT_FIELD(fieldValue, fieldWidth, fieldFirstBit)        ((((uint32)(fieldValue)) & ((1 << (fieldWidth)) - 1)) << (fieldFirstBit))
+#define GRLIB_FLAG_MASK(bitPosition)                                    (1 << (bitPosition))
+
+
+/*
+ *
+ * GRLIB PNP TABLE DEFINITIONS
+ *
+ * */
+
+#define GRLIB_PNP_VENDOR_ID_ANY                          0x00
+#define GRLIB_PNP_VENDOR_ID_GAISLER_RESEARCH             0x01
+#define GRLIB_PNP_VENDOR_ID_PENDER_ELECTRONIC_DESIGN     0x02
+#define GRLIB_PNP_VENDOR_ID_EUROPEAN_SPACE_AGENCY        0x04
+#define GRLIB_PNP_VENDOR_ID_ASTRIUM_EADS                 0x06
+#define GRLIB_PNP_VENDOR_ID_OPEN_CHIP                    0x07
+#define GRLIB_PNP_VENDOR_ID_OPEN_CORES                   0x08
+#define GRLIB_PNP_VENDOR_ID_VARIOUS_CONTRIBUTIONS        0x09
+#define GRLIB_PNP_VENDOR_ID_DLR                          0x0a
+
+#define GRLIB_PNP_DEVICE_ID_ANY                          0x00
+#define GRLIB_PNP_DEVICE_ID_APBCTRL                      0x06
+#define GRLIB_PNP_DEVICE_ID_IRQMP                        0x0d
+#define GRLIB_PNP_DEVICE_ID_GPTIMER                      0x11
+
+#define GRLIB_PNP_BAR_ENTRY_TYPE_APBIOSPACE              0x01
+#define GRLIB_PNP_BAR_ENTRY_TYPE_AHBMEMORYSPACE          0x02
+#define GRLIB_PNP_BAR_ENTRY_TYPE_AHBIOSPACE              0x03
+
+
+/*
+ *
+ * REGISTER OFFSETS
+ *
+ * */
+
 #define IRQMP_INTERRUPT_LEVEL_REGISTER                       (0x00)
 #define IRQMP_INTERRUPT_PENDING_REGISTER                     (0x04)
 #define IRQMP_INTERRUPT_FORCE_REGISTER                       (0x08)
@@ -81,24 +124,59 @@
 #define GRLIB_GPTIMER_LATCH_REGISTER(timerIndex)         (0x10 * (timerIndex + 1) + 0x0c)
 
 
-#define GRLIB_PNP_VENDOR_ID_ANY                      0x00
-#define GRLIB_PNP_VENDOR_ID_GAISLER_RESEARCH         0x01
-#define GRLIB_PNP_VENDOR_ID_PENDER_ELECTRONIC_DESIGN 0x02
-#define GRLIB_PNP_VENDOR_ID_EUROPEAN_SPACE_AGENCY    0x04
-#define GRLIB_PNP_VENDOR_ID_ASTRIUM_EADS             0x06
-#define GRLIB_PNP_VENDOR_ID_OPEN_CHIP                0x07
-#define GRLIB_PNP_VENDOR_ID_OPEN_CORES               0x08
-#define GRLIB_PNP_VENDOR_ID_VARIOUS_CONTRIBUTIONS    0x09
-#define GRLIB_PNP_VENDOR_ID_DLR                      0x0a
+#define GRLIB_APBUART_DATA_REGISTER                      (0x00)
+#define GRLIB_APBUART_STATUS_REGISTER                    (0x04)
+#define GRLIB_APBUART_CONTROL_REGISTER                   (0x08)
+#define GRLIB_APBUART_SCALER_REGISTER                    (0x0c)
+#define GRLIB_APBUART_FIFO_DEBUG_REGISTER                (0x10)
 
-#define GRLIB_PNP_DEVICE_ID_ANY                      0x00
-#define GRLIB_PNP_DEVICE_ID_APBCTRL                  0x06
-#define GRLIB_PNP_DEVICE_ID_IRQMP                    0x0d
-#define GRLIB_PNP_DEVICE_ID_GPTIMER                  0x11
 
-#define GRLIB_PNP_BAR_ENTRY_TYPE_APBIOSPACE          0x01
-#define GRLIB_PNP_BAR_ENTRY_TYPE_AHBMEMORYSPACE      0x02
-#define GRLIB_PNP_BAR_ENTRY_TYPE_AHBIOSPACE          0x03
+/*
+ *
+ * APBUART REGISTER CONTENT DEFINITIONS
+ *
+ * */
+
+#define GRLIB_GET_FIELD_APBUART_DATA_REGISTER_DATA(registerValue)           (GRLIB_GET_FIELD(registerValue,  8,  0))
+#define GRLIB_GET_FIELD_APBUART_STATUS_REGISTER_RCNT(registerValue)         (GRLIB_GET_FIELD(registerValue,  6, 26))
+#define GRLIB_GET_FIELD_APBUART_STATUS_REGISTER_TCNT(registerValue)         (GRLIB_GET_FIELD(registerValue,  6, 20))
+#define GRLIB_GET_FIELD_APBUART_SCALER_REGISTER_RELOAD_VALUE(registerValue) (GRLIB_GET_FIELD(registerValue, 31,  0))
+#define GRLIB_GET_FIELD_APBUART_FIFO_DEBUG_REGISTER_DATA(registerValue)     (GRLIB_GET_FIELD(registerValue,  8,  0))
+
+#define GRLIB_SHIFT_FIELD_APBUART_DATA_REGISTER_VALUE(fieldValue)           (GRLIB_SHIFT_FIELD(fieldValue,  8,  0))
+#define GRLIB_SHIFT_FIELD_APBUART_STATUS_REGISTER_RCNT(fieldValue)          (GRLIB_SHIFT_FIELD(fieldValue,  6, 26))
+#define GRLIB_SHIFT_FIELD_APBUART_STATUS_REGISTER_TCNT(fieldValue)          (GRLIB_SHIFT_FIELD(fieldValue,  6, 20))
+#define GRLIB_SHIFT_FIELD_APBUART_SCALER_REGISTER_RELOAD_VALUE(fieldValue)  (GRLIB_SHIFT_FIELD(fieldValue, 31,  0))
+#define GRLIB_SHIFT_FIELD_APBUART_FIFO_DEBUG_REGISTER_VALUE(fieldValue)     (GRLIB_SHIFT_FIELD(fieldValue,  8,  0))
+
+#define GRLIB_FLAG_MASK_APBUART_STATUS_REGISTER_RF                          (GRLIB_FLAG_MASK(10))
+#define GRLIB_FLAG_MASK_APBUART_STATUS_REGISTER_TF                          (GRLIB_FLAG_MASK( 9))
+#define GRLIB_FLAG_MASK_APBUART_STATUS_REGISTER_RH                          (GRLIB_FLAG_MASK( 8))
+#define GRLIB_FLAG_MASK_APBUART_STATUS_REGISTER_TH                          (GRLIB_FLAG_MASK( 7))
+#define GRLIB_FLAG_MASK_APBUART_STATUS_REGISTER_FE                          (GRLIB_FLAG_MASK( 6))
+#define GRLIB_FLAG_MASK_APBUART_STATUS_REGISTER_PE                          (GRLIB_FLAG_MASK( 5))
+#define GRLIB_FLAG_MASK_APBUART_STATUS_REGISTER_OV                          (GRLIB_FLAG_MASK( 4))
+#define GRLIB_FLAG_MASK_APBUART_STATUS_REGISTER_BR                          (GRLIB_FLAG_MASK( 3))
+#define GRLIB_FLAG_MASK_APBUART_STATUS_REGISTER_TE                          (GRLIB_FLAG_MASK( 2))
+#define GRLIB_FLAG_MASK_APBUART_STATUS_REGISTER_TS                          (GRLIB_FLAG_MASK( 1))
+#define GRLIB_FLAG_MASK_APBUART_STATUS_REGISTER_DR                          (GRLIB_FLAG_MASK( 0))
+#define GRLIB_FLAG_MASK_APBUART_CONTROL_REGISTER_FA                         (GRLIB_FLAG_MASK(31))
+#define GRLIB_FLAG_MASK_APBUART_CONTROL_REGISTER_NS                         (GRLIB_FLAG_MASK(15))
+#define GRLIB_FLAG_MASK_APBUART_CONTROL_REGISTER_SI                         (GRLIB_FLAG_MASK(14))
+#define GRLIB_FLAG_MASK_APBUART_CONTROL_REGISTER_DI                         (GRLIB_FLAG_MASK(13))
+#define GRLIB_FLAG_MASK_APBUART_CONTROL_REGISTER_BI                         (GRLIB_FLAG_MASK(12))
+#define GRLIB_FLAG_MASK_APBUART_CONTROL_REGISTER_DB                         (GRLIB_FLAG_MASK(11))
+#define GRLIB_FLAG_MASK_APBUART_CONTROL_REGISTER_RF                         (GRLIB_FLAG_MASK(10))
+#define GRLIB_FLAG_MASK_APBUART_CONTROL_REGISTER_TF                         (GRLIB_FLAG_MASK( 9))
+#define GRLIB_FLAG_MASK_APBUART_CONTROL_REGISTER_EC                         (GRLIB_FLAG_MASK( 8))
+#define GRLIB_FLAG_MASK_APBUART_CONTROL_REGISTER_LB                         (GRLIB_FLAG_MASK( 7))
+#define GRLIB_FLAG_MASK_APBUART_CONTROL_REGISTER_FL                         (GRLIB_FLAG_MASK( 6))
+#define GRLIB_FLAG_MASK_APBUART_CONTROL_REGISTER_PE                         (GRLIB_FLAG_MASK( 5))
+#define GRLIB_FLAG_MASK_APBUART_CONTROL_REGISTER_PS                         (GRLIB_FLAG_MASK( 4))
+#define GRLIB_FLAG_MASK_APBUART_CONTROL_REGISTER_TI                         (GRLIB_FLAG_MASK( 3))
+#define GRLIB_FLAG_MASK_APBUART_CONTROL_REGISTER_RI                         (GRLIB_FLAG_MASK( 2))
+#define GRLIB_FLAG_MASK_APBUART_CONTROL_REGISTER_TE                         (GRLIB_FLAG_MASK( 1))
+#define GRLIB_FLAG_MASK_APBUART_CONTROL_REGISTER_RE                         (GRLIB_FLAG_MASK( 0))
 
 
 /*==================[typedef]================================================*/
