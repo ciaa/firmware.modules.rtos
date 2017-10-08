86f26bec31b2681af8aa753713e4443be3873fc2
Gerardo Puga <glpuga@gmail.com>
2017-10-08 09:35:04 -0300

    Fixed a violation of the style format for header files.

--- inc/cortexM4/StartOs_Arch_SysTick.h
+++ inc/cortexM4/StartOs_Arch_SysTick.h
@@ -1,5 +1,84 @@
+/* Copyright 2014, Pablo Ridolfi (UTN-FRBA)
+ *
+ * This file is part of CIAA Firmware.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
 
-/* FIXME This file should either follow the same template as the rest of the rtos, or be deleted and its
- * functionality included somewhere else. */
+#ifndef _START_OS_ARCH_SYSTICK_H_
+#define _START_OS_ARCH_SYSTICK_H_
+
+/** \brief FreeOSEK Os Architecture Dependent Header File
+ **
+ ** This file is included form os.h and defines macros
+ ** and types which depends on the architecture.
+ **
+ ** \file cortexM4/Start_Os_Arch_Systick.h
+ ** \arch cortexM4
+ **
+ **/
+
+/** \addtogroup FreeOSEK
+ ** @{ */
+/** \addtogroup FreeOSEK_Os
+ ** @{ */
+/** \addtogroup FreeOSEK_Os_Global
+ ** @{ */
+
+
+
+/*==================[inclusions]=============================================*/
+
+
+
+/*==================[macros]=================================================*/
+
+
+
+/*==================[typedef]================================================*/
+
+
+
+/*==================[external data declaration]==============================*/
+
+
+
+/*==================[external functions declaration]=========================*/
+
+
 
 void StartOs_Arch_SysTick(void);
+
+
+
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/** @} doxygen end group definition */
+/*==================[end of file]============================================*/
+#endif /* #ifndef _START_OS_ARCH_SYSTICK_H_ */
+
