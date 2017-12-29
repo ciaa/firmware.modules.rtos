29655aa45ca5bec70a75c9e0757953b4147ea05b
Gerardo Puga <glpuga@gmail.com>
2017-12-29 15:26:25 -0300

    Improved legibility and corrected typos in comments on the CortexM0 code.

--- src/cortexM0/Os_Arch.c
+++ src/cortexM0/Os_Arch.c
@@ -44,22 +44,43 @@
 /** \addtogroup FreeOSEK_Os_Global
  ** @{ */
 
+
+
 /*==================[inclusions]=============================================*/
+
+
+
 #include "Os_Internal.h"
+
+
 
 /*==================[macros and definitions]=================================*/
 
+
+
 /*==================[internal data declaration]==============================*/
+
+
 
 /*==================[internal functions declaration]=========================*/
 
+
+
 /*==================[internal data definition]===============================*/
+
+
 
 /*==================[external data definition]===============================*/
 
+
+
 /*==================[internal functions definition]==========================*/
 
+
+
 /*==================[external functions definition]==========================*/
+
+
 
 /** @} doxygen end group definition */
 /** @} doxygen end group definition */
